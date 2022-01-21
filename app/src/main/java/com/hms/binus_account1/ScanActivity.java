6ee816e207ac0ff854802954c413450fb80d01e7
Hafiz Elfia Wedo Putra <hafizelfiawedoputra@gmail.com>
2022-01-21 19:22:50 +0700

    initial commit

---
+++ app/src/main/java/com/hms/binus_account1/ScanActivity.java
@@ -0,0 +1,125 @@
+package com.hms.binus_account1;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.huawei.hms.support.hwid.ui.HuaweiIdAuthButton;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
+
+public class ScanActivity extends AppCompatActivity {
+
+    Button button_capture, button_copy;
+    TextView textview_data;
+    Bitmap bitmap;
+    private static final int REQUEST_CAMERA_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        button_capture = findViewById(R.id.button_capture);
+        button_copy = findViewById(R.id.button_copy);
+        textview_data = findViewById(R.id.text_data);
+        Button buttonLogout = findViewById(R.id.logout);
+
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ScanActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(ScanActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(ScanActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, REQUEST_CAMERA_CODE);
+        }
+
+        button_capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(ScanActivity.this);
+
+            }
+        });
+
+        button_copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String scanned_text = textview_data.getText().toString();
+                copyToClipBoard(scanned_text);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK){
+                Uri resultUri = result.getUri();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                    getTextFromImage(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    private void getTextFromImage(Bitmap bitmap){
+        TextRecognizer recognizer = new TextRecognizer.Builder(this).build();
+        if (!recognizer.isOperational()){
+            Toast.makeText(ScanActivity.this, "Error Occurred!", Toast.LENGTH_SHORT);
+        }
+        else {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> textBlockSparseArray = recognizer.detect(frame);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i=0; i<textBlockSparseArray.size(); i++){
+                TextBlock textBlock = textBlockSparseArray.valueAt(i);
+                stringBuilder.append(textBlock.getValue());
+                stringBuilder.append("\n");
+            }
+            textview_data.setText(stringBuilder.toString());
+            button_capture.setText("Retake");
+            button_copy.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void copyToClipBoard(String text){
+        ClipboardManager clipBoard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Copied data", text);
+        clipBoard.setPrimaryClip(clip);
+        Toast.makeText(ScanActivity.this, "Copied to clipboard!", Toast.LENGTH_SHORT).show();
+
+    }
+}
